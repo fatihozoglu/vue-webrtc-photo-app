cb45710426eef18f1c7af388a3193c43c5127dcf
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-12-30 18:03:48 +0300

    Add startup and takepicture methods to take snapshot from the MediaStream

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,97 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="camera">
+      <video ref="video" :srcObject.prop="videoSource" id="video" autoplay>
+        Video stream not available.
+      </video>
+      <button @click="takepicture" id="startbutton">Take Photo</button>
+    </div>
+    <canvas ref="canvas" id="canvas" />
+    <div class="output">
+      <img
+        :src="photoSource"
+        id="photo"
+        alt="The screen capture will appear in this box."
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      videoSource: null,
+      photoSource: null,
+    };
+  },
+  methods: {
+    startup() {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false })
+        .then((stream) => {
+          this.videoSource = stream;
+        })
+        .catch((err) => {
+          console.log("An error occurred: " + err);
+        });
+    },
+    takepicture() {
+      let context = this.$refs.canvas.getContext("2d");
+      context.drawImage(this.$refs.video, 0, 0, 200, 200);
+      let data = this.$refs.canvas.toDataURL("image/png");
+      this.photoSource = data;
+    },
+  },
+  mounted() {
+    this.startup();
   },
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  padding: 2rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 2rem;
+}
+
+#canvas {
+  display: none;
+}
+
+.camera {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  align-items: center;
+  grid-column: 1 / 2;
+}
+
+#startbutton {
+  font-size: 1rem;
+  padding: 1rem 2rem;
+  border: none;
+  outline: none;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.output {
+  grid-column: 2 / 3;
+  background-color: tan;
 }
 </style>
